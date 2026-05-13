3acfbbfc4275ab3314c4f6919a7eef2f1f4eb482
Modesto Felipe <fekimodesto04@gmail.com>
2026-05-13 08:28:24 -0300

    feat: Updated app.py to make Render work.

--- api/app.py
+++ api/app.py
@@ -129,4 +129,6 @@ def hora_de_pico():
         return jsonify({"erro": str(e)}), 500
 
 if __name__ == "__main__":
-    app.run(debug=True, port=5000)
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
